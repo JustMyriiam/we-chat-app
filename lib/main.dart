af12ea1c17e459a0160481c50ce2e474c4a23b01
JustMyriiam <mariemsghaier77@gmail.com>
2024-07-03 00:13:22 +0100

    change firebase initialization in main app

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,12 @@
-import 'dart:io';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:we_chat_app/auth/auth_gate.dart';
+import 'package:we_chat_app/firebase_options.dart';
 import 'package:we_chat_app/themes/light_mode.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Platform.isAndroid?
-  await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: "AIzaSyDzXYqKG12XjjAAcdrWleIBsN9IeHqBBe8",
-      appId: "1:605546278260:android:a29acd76f8ba65a13d1021",
-      messagingSenderId: "605546278260",
-     projectId: "wechatapp-423f5",
-      ),
-  )
-  :await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp()); 
 }
 
